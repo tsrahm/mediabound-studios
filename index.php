90055012a251df6447e13c0a39c978aa11bd0a72
Torsten <torsten@AnniesMacLaptop.local>
2016-10-23 14:13:03 -0600

    filepath fix 4

--- index.php
+++ index.php
@@ -5,75 +5,14 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link href="https://fonts.googleapis.com/css?family=Oxygen|Slabo+27px|Taviraj:400,500,600,700,800,900" rel="stylesheet">
-    <link rel="shortcut icon" type="image/x-icon" href="/images/animal-paw-print_green-t.png">
-    <link rel="stylesheet" type="text/css" href="./css/index.css">
-    <script src="https://use.fontawesome.com/d343e84307.js"></script>
-<!--
-    <link rel="stylesheet" type="text/css" href="/css/bootstrap-custom2.min.css">
-    <script type="text/javascript" async="async" defer="defer" src="/application.js"></script>
--->
+
   </head>
   <body>
-    <div class="container">
-      <header>
-        <?php include '../resources/templates/header.php'; ?>
-      </header>
-      <main>
-        <section class="main-container-home">
-          <div class="photo-container">
-            <div class="photo-clipper">
-              <img class="photo" src="../resources/library/images/jets.png">
-            </div>
-            <h3>Why Us?</h3>
-            <p>Description here.</p>
-          </div>
-          <div class="photo-container">
-            <div class="photo-clipper">
-              <img class="photo" src="../resources/library/images/lizard.png">
-            </div>
-            <h3>What We Can Do For You</h3>
-            <p>Description here.</p>
-          </div>
-          <div class="photo-container">
-            <div class="photo-clipper">
-              <img class="photo" src="../resources/library/images/bubbles.png">
-            </div>
-            <h3>Current Projects</h3>
-            <p>Description here.</p>
-          </div>
-        </section>
-        <section class="icon-container-home">
-          <span class="dot dot-1"></span>
-          <span class="dot dot-2"></span>
-          <span class="dot dot-3"></span>
-          <div class="icon-container">
-            <span class="fa fa-fast-backward fa-border content-icon"></span>
-            <h4>TITLE</h4>
-          </div><!-- 
-       --><div class="icon-container">
-            <span class="fa fa-backward fa-border content-icon"></span>
-            <h4>TITLE</h4>
-          </div><!-- 
-       --><div class="icon-container">
-            <span class="fa fa-play fa-border content-icon"></span>
-            <h4>TITLE</h4>
-          </div><!-- 
-       --><div class="icon-container">
-            <span class="fa fa-forward fa-border content-icon"></span>
-            <h4>TITLE</h4>
-          </div><!-- 
-       --><div class="icon-container">
-            <span class="fa fa-fast-forward fa-border content-icon"></span>
-            <h4>TITLE</h4>
-          </div>
-        </section>
-      </main>
-      <footer class="media-bound-footer">
-        <?php include '../resources/templates/footer.php'; ?>
-      </footer>
-    </div>
-    <script type="text/javascript" async="async" defer="defer" src="./js/header.js"></script>
+    <header></header>
+    <main>
+      <a href="./public_html/index.php"></a>
+    </main>
+    <footer></footer>
   </body>
 </html>
 
