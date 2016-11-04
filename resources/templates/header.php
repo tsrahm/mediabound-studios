ccf066bc9fc7c4bb52535654180092b39b77f9f6
Torsten <torsten@AnniesMacLaptop.local>
2016-11-04 17:03:13 -0600

    Made other pages responsive

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -1,76 +1,72 @@
-<div class="header-branding">
+<div class="header-container">
   <img class="logo" src="<?= $resources; ?>/library/images/MBS-Logo-Full.png" />
+  <ul class="header-contact">
+    <li>sales@mediabound.com</li>
+    <li class="spacer"><span>|</span></li>
+    <li>303.222.2222</li>
+  </ul>
+  <nav class="large-nav">
+    <ul class="nav-headers">
+      <li>
+        <a href="<?= $siteroot; ?>/index.php">Home</a></li>
+      <li class="spacer"><span>|</span></li>
+      <li class="nav-header-dropdown">
+        <a cursor="pointer" href="#">About</a>
+        <ul class="nav-sub-headers nav-sub-about">
+          <li><a href="#">Sizzle Reel</a></li>
+          <li><a href="#">Why Video</a></li>
+          <li><a href="#">Testimonials</a></li>
+          <li><a href="#">Our Philosophy</a></li>
+        </ul>
+      </li>
+      <li class="spacer"><span>|</span></li>
+      <li class="nav-header-dropdown">
+        <a href="#">Work</a>
+        <ul class="nav-sub-headers nav-sub-work">
+          <li><a href="<?= $siteroot; ?>/corporate-videos.php">Corporate Videos</a></li>
+          <li><a href="#">Web Series Videos</a></li>
+          <li><a href="#">Commercials</a></li>
+          <li><a href="#">Documentaries</a></li>
+          <li><a href="#">Instructional</a></li>
+          <li><a href="#">Live Events</a></li>
+        </ul>
+      </li>
+      <li class="spacer"><span>|</span></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+
+
+  <div class="small-nav-icon fa fa-list-ul fa-border"></div>
+
+
+  <nav class="small-nav">
+    <div class="nav-close fa fa-times fa-border"></div>
+    <ul class="nav-container">
+      <li><a href="<?= $siteroot; ?>/index.php">Home</a></li>
+      <li class="nav-header">
+        <a cursor="pointer" href="#">About</a>
+        <ul class="nav-sub-header">
+          <li><a href="#">Sizzle Reel</a></li>
+          <li><a href="#">Why Video</a></li>
+          <li><a href="#">Testimonials</a></li>
+          <li><a href="#">Our Philosophy</a></li>
+        </ul>
+      </li>
+      <li class="nav-header">
+        <a href="#">Work</a>
+        <ul class="nav-sub-header">
+          <li><a href="<?= $siteroot; ?>/corporate-videos.php">Corporate Videos</a></li>
+          <li><a href="#">Web Series Videos</a></li>
+          <li><a href="#">Commercials</a></li>
+          <li><a href="#">Documentaries</a></li>
+          <li><a href="#">Instructional</a></li>
+          <li><a href="#">Live Events</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
 </div>
-<ul class="header-contact">
-  <li>303.222.2222</li>
-  <li class="spacer"><span>|</span></li>
-  <li>sales@mediabound.com</li>
-</ul>
-<nav class="large-nav">
-  <ul class="nav-headers">
-    <li>
-      <a href="<?= $siteroot; ?>/index.php">Home</a></li>
-    <li class="spacer"><span>|</span></li>
-    <li class="nav-header-dropdown">
-      <a cursor="pointer" href="#">About</a>
-      <ul class="nav-sub-headers nav-sub-about">
-        <li><a href="#">Sizzle Reel</a></li>
-        <li><a href="#">Why Video</a></li>
-        <li><a href="#">Testimonials</a></li>
-        <li><a href="#">Our Philosophy</a></li>
-      </ul>
-    </li>
-    <li class="spacer"><span>|</span></li>
-    <li class="nav-header-dropdown">
-      <a href="#">Work</a>
-      <ul class="nav-sub-headers nav-sub-work">
-        <li><a href="<?= $siteroot; ?>/corporate-videos.php">Corporate Videos</a></li>
-        <li><a href="#">Web Series Videos</a></li>
-        <li><a href="#">Commercials</a></li>
-        <li><a href="#">Documentaries</a></li>
-        <li><a href="#">Instructional</a></li>
-        <li><a href="#">Live Events</a></li>
-      </ul>
-    </li>
-    <li class="spacer"><span>|</span></li>
-    <li><a href="#">Contact</a></li>
-  </ul>
-</nav>
-
-
-
-
-
-
-<div class="small-nav-icon fa fa-list-ul fa-border"></div>
-
-<nav class="small-nav">
-  <div class="nav-close fa fa-times fa-border"></div>
-  <ul class="nav-container">
-    <li><a href="<?= $siteroot; ?>/index.php">Home</a></li>
-    <li class="nav-header">
-      <a cursor="pointer" href="#">About</a>
-      <ul class="nav-sub-header">
-        <li><a href="#">Sizzle Reel</a></li>
-        <li><a href="#">Why Video</a></li>
-        <li><a href="#">Testimonials</a></li>
-        <li><a href="#">Our Philosophy</a></li>
-      </ul>
-    </li>
-    <li class="nav-header">
-      <a href="#">Work</a>
-      <ul class="nav-sub-header">
-        <li><a href="<?= $siteroot; ?>/corporate-videos.php">Corporate Videos</a></li>
-        <li><a href="#">Web Series Videos</a></li>
-        <li><a href="#">Commercials</a></li>
-        <li><a href="#">Documentaries</a></li>
-        <li><a href="#">Instructional</a></li>
-        <li><a href="#">Live Events</a></li>
-      </ul>
-    </li>
-    <li><a href="#">Contact</a></li>
-  </ul>
-</nav>
-
 
 
